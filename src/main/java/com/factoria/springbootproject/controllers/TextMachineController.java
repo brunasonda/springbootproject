fa87ba5824b0e0c4d927d08d28ff4893e1be3ab8
Bruna Sonda <bsonda@gmail.com>
2025-06-11 14:03:36 +0200

    Add show features

--- src/main/java/com/factoria/springbootproject/controllers/TextMachineController.java
+++ src/main/java/com/factoria/springbootproject/controllers/TextMachineController.java
@@ -1,4 +1,18 @@
 package com.factoria.springbootproject.controllers;
+import com.factoria.springbootproject.models.TextMachine;
+import com.factoria.springbootproject.services.TextMachineService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 public class TextMachineController {
+    private final TextMachineService textMachineService;
+
+    public TextMachineController(TextMachineService textMachineService) {
+        this.textMachineService = textMachineService;
+    }
+
+    @GetMapping("/lista") //Listar
+    public List<TextMachine> getAllTextMachine() {
+        return textMachineService.getAllTextMachine();
+    }
 }
