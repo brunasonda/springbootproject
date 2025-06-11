8ff67fa86c9388a913760eeb302a873118102912
Bruna Sonda <bsonda@gmail.com>
2025-06-11 13:20:56 +0200

    Seventh Commit: Add TextMachineController

--- src/main/java/com/factoria/springbootproject/controllers/TextMachineController.java
+++ src/main/java/com/factoria/springbootproject/controllers/TextMachineController.java
@@ -1,4 +1,12 @@
 package com.factoria.springbootproject.controllers;
+import com.factoria.springbootproject.models.TextMachine;
+import com.factoria.springbootproject.services.TextMachineService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 public class TextMachineController {
+    private final TextMachineService textMachineService;
+
+    public TextMachineController(TextMachineService textMachineService) {
+        this.textMachineService = textMachineService;
 }
